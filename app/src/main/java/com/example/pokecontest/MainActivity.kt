3dbee4f211b000ed9a9d9c7de356bdc43996918f
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-01-23 12:28:59 -0600

    Comienza a editar el MainActivity

--- app/src/main/java/com/example/pokecontest/MainActivity.kt
+++ app/src/main/java/com/example/pokecontest/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.pokecontest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pokecontest.adapters.PokemonAdapter
+import com.example.pokecontest.listeners.PokemonListener
+import com.example.pokecontest.models.Pokemon
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PokemonListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val pokemones = arrayListOf<Pokemon>(
+        Pokemon(1, "Mew","Psíquico","Kanto", 50),
+        Pokemon(2,"Pichu", "Eléctrico", "Jhotto", 2),
+        Pokemon(3,"Ponyta", "Hada", "Galar", 30),
+        Pokemon(4,"Leafon","Planta","Jhotto", 24)
+    )
+
+    val adaptador = PokemonAdapter(pokemones)
+
+    override fun onClickPokemon(pokemon: Pokemon) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInfoPokemon(pokemon: Pokemon) {
+        TODO("Not yet implemented")
+    }
+
+
 }
