d25d41d97f91144d89ffced5edafbcd8708da8a5
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-01-23 12:06:26 -0600

    Implementación básica del PokemonViewHolder

--- app/src/main/java/com/example/pokecontest/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokecontest/adapters/PokemonAdapter.kt
@@ -1,4 +1,21 @@
 package com.example.pokecontest.adapters
 
-class PokemonAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokecontest.adapters.viewholders.PokemonViewHolder
+import com.example.pokecontest.models.Pokemon
+
+class PokemonAdapter(val pokemones: List<Pokemon>): RecyclerView.Adapter<PokemonViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/example/pokecontest/adapters/viewholders/PokemonViewHolder.kt
+++ app/src/main/java/com/example/pokecontest/adapters/viewholders/PokemonViewHolder.kt
@@ -1,4 +1,19 @@
 package com.example.pokecontest.adapters.viewholders
 
-class PokemonViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokecontest.listeners.PokemonListener
+import com.example.pokecontest.models.Pokemon
+
+class PokemonViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView) {
+    private var pokemonListener: PokemonListener? = null
+    private var pokemon: Pokemon? = null
+
+    fun bindPokemon(pokemon: Pokemon) {
+        this.pokemon = pokemon
+    }
+
+    fun setPokemonListener(listener: PokemonListener?) {
+        this.pokemonListener = listener
+    }
 }
