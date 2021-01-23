ffc2f2cc018bb3db1a97691081148b584129ae81
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-01-23 12:18:04 -0600

    Termina implementación del PokemonAdapter

--- app/src/main/java/com/example/pokecontest/adapters/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokecontest/adapters/PokemonAdapter.kt
@@ -1,21 +1,29 @@
 package com.example.pokecontest.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokecontest.R
 import com.example.pokecontest.adapters.viewholders.PokemonViewHolder
+import com.example.pokecontest.listeners.PokemonListener
 import com.example.pokecontest.models.Pokemon
 
-class PokemonAdapter(val pokemones: List<Pokemon>): RecyclerView.Adapter<PokemonViewHolder>() {
+class PokemonAdapter(private val pokemones: List<Pokemon>): RecyclerView.Adapter<PokemonViewHolder>() {
+    private var pokemonListener: PokemonListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent,false)
+        val pokemonViewHolder = PokemonViewHolder(itemView)
+        pokemonViewHolder.setPokemonListener(pokemonListener)
+        return pokemonViewHolder
     }
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindPokemon(pokemones[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return pokemones.size
     }
 
 }
