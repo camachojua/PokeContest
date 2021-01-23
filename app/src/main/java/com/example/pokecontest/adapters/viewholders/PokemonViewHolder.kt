6b8f46fd3aaaed5cb582b1f4b673740f257eee84
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-01-23 12:12:50 -0600

    Termina implementacion del PokemonViewHolder

--- app/src/main/java/com/example/pokecontest/adapters/viewholders/PokemonViewHolder.kt
+++ app/src/main/java/com/example/pokecontest/adapters/viewholders/PokemonViewHolder.kt
@@ -1,7 +1,11 @@
 package com.example.pokecontest.adapters.viewholders
 
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokecontest.R
 import com.example.pokecontest.listeners.PokemonListener
 import com.example.pokecontest.models.Pokemon
 
@@ -9,8 +13,24 @@ class PokemonViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView) {
     private var pokemonListener: PokemonListener? = null
     private var pokemon: Pokemon? = null
 
+    private var pokemonNameTextView: TextView = itemView.findViewById(R.id.nombre)
+    private var pokemonTypeTextView: TextView = itemView.findViewById(R.id.tipo)
+    private var pokemonPhotoImageView: ImageView = itemView.findViewById(R.id.foto)
+    private var infoPokemon: Button = itemView.findViewById(R.id.button_info)
+
     fun bindPokemon(pokemon: Pokemon) {
         this.pokemon = pokemon
+        pokemonNameTextView.text = this.pokemon?.nombre
+        pokemonTypeTextView.text = this.pokemon?.tipo
+        pokemonPhotoImageView.id = this.pokemon?.fotoId!!
+
+        itemView.setOnClickListener { view ->
+            pokemonListener?.onClickPokemon(this.pokemon!!)
+        }
+
+        infoPokemon.setOnClickListener { view ->
+            pokemonListener?.onInfoPokemon(this.pokemon!!)
+        }
     }
 
     fun setPokemonListener(listener: PokemonListener?) {
